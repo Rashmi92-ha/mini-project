ff2bfb37671d27b85a140b48c68f2a563b5dde10
Rashmi KS <rashmiks727@gmail.com>
2026-06-03 23:02:20 +0530

    Implemented employee management app with CRUD operations and API Integration #7

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getEmployee(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+  }
+
+  addEmployee(employee: Employee): Observable<Employee> {
+    return this.httpClient.post<Employee>(
+      'https://jsonplaceholder.typicode.com/users',
+      employee,
+    );
+  }
 }
