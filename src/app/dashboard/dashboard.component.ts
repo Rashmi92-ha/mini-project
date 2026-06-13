fb22caefbc539f9d8fefee4047f350fddf252980
Rashmi KS <rashmiks727@gmail.com>
2026-06-13 18:57:08 +0530

    fet: display total employee count on dashboard #21

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { EmployeeService } from '../services/employee.service';
+import { Employee } from '../models/employee';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +11,24 @@ import { Router } from '@angular/router';
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-  constructor(private router: Router) {}
+  employeeList: Employee[] = [];
+  totalEmployee = 0;
+  totalEmail = 0;
+
+  constructor(
+    private router: Router,
+    private employeeService: EmployeeService,
+  ) {}
+
+  ngOnInit() {
+    this.employeeService.getEmployee().subscribe((data) => {
+      this.employeeList = data;
+      this.totalEmployee = this.employeeList.length;
+      this.totalEmail = this.employeeList.filter(
+        (employee) => employee.email,
+      ).length;
+    });
+  }
 
   logout() {
     localStorage.removeItem('isLoggedIn');
