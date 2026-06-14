5c8788aadfeec3022f807ee2ede620f1167128dc
Rashmi KS <rashmiks727@gmail.com>
2026-06-14 12:21:47 +0530

    Implement Employee Data Table #23

--- src/app/models/employee.ts
+++ src/app/models/employee.ts
@@ -1,8 +1,11 @@
-export interface Employee{
-    id? : number;
-    name : string;
-    email : string;
-    phone : string;
-    designation : string;
-    salary : number | string;
+export interface Employee {
+  id?: number;
+  name: string;
+  email: string;
+  phone: string;
+  company?: {
+    name: string;
+  };
+  designation: string;
+  salary: number | string;
 }
